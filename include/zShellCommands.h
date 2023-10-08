f97e88acb70b38624fd94365672d2fd379986ba6
Torsten Hemm <t.hemm@gmx.de>
2023-10-08 18:59:34 +0200

    sysinfo erweitert

--- include/zShellCommands.h
+++ include/zShellCommands.h
@@ -7,32 +7,27 @@ void free(int argc, char *argv)
      Serial.println("Free memory: " + String(esp_get_free_heap_size()) + " bytes");
 }
 
+/**
+ * @brief: gibt systeminformationen aus
+ **/
 void sysinfo(int argc, char *argv)
 {
-     uint32_t chipId = 0;
+     
 
+     // Chipmodel
+     uint32_t chipId = 0;
      for (int i = 0; i < 17; i = i + 8)
      {
           chipId |= ((ESP.getEfuseMac() >> (40 - i)) & 0xff) << i;
      }
-
-     Serial.printf("ESP32 Chip model = %s Rev %d\n", ESP.getChipModel(), ESP.getChipRevision());
-     Serial.printf("This chip has %d cores\n", ESP.getChipCores());
-     Serial.print("Chip ID: ");
-     Serial.println(chipId);
-
-     uint32_t Freq = getCpuFrequencyMhz();
-     Serial.print("CPU Freq = ");
-     Serial.print(Freq);
-     Serial.println(" MHz");
-     Freq = getXtalFrequencyMhz();
-     Serial.print("XTAL Freq = ");
-     Serial.print(Freq);
-     Serial.println(" MHz");
-     Freq = getApbFrequency();
-     Serial.print("APB Freq = ");
-     Serial.print(Freq);
-     Serial.println(" Hz");
+     Serial.printf("Chip model   :\t %s Rev %d\n", ESP.getChipModel(), ESP.getChipRevision());
+     Serial.printf("Anzahl Kerne :\t %d \n", ESP.getChipCores());
+     Serial.printf("Chip ID      :\t %d \n", chipId);
+     Serial.printf("CPU Freq.    :\t %d \n Mhz", ESP.getCpuFreqMHz());
+     Serial.printf("Max  DRAM    :\t %d \n Byte", ESP.getHeapSize());
+     Serial.printf("Free DRAM    :\t %d \n Byte", ESP.getFreeHeap());
+     Serial.printf("Max  PSRAM   :\t %d \n Byte", ESP.getPsramSize());
+     Serial.printf("Free PSRAM   :\t %d \n Byte", ESP.getFreePsram());
 }
 
 #endif
